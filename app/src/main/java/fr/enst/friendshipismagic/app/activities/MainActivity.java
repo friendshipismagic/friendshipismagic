ef3b53528bf4056e1be0294ced23af09dd816216
Alexandre JANNIAUX <alexandre.janniaux@gmail.com>
2016-06-17 10:37:04 +0200

    Update gradle build.

--- app/src/main/java/fr/enst/friendshipismagic/app/activities/MainActivity.java
+++ app/src/main/java/fr/enst/friendshipismagic/app/activities/MainActivity.java
@@ -1,8 +1,12 @@
 package fr.enst.friendshipismagic.app.activities;
 
 import android.app.Activity;
+import android.app.NativeActivity;
 import android.os.Bundle;
-
+import org.jsfml.*;
+import org.jsfml.graphics.RenderWindow;
+import org.jsfml.window.VideoMode;
+import org.jsfml.window.event.Event;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +14,23 @@ public class MainActivity extends Activity {
     @Override
     public void onCreate(Bundle state) {
         super.onCreate(state);
+        RenderWindow window = new RenderWindow();//VideoMode.getDesktopMode(), "");
+
+        while (window.isOpen()) {
+
+            for(Event event : window.pollEvents()) {
+                if (event.type == Event.Type.CLOSED)
+                    window.close();
+            }
+
+
+            window.clear();
+
+            window.display();
+
+        }
+
+
         //setContentView(new TextView("It works"));
     }
 
