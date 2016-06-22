0f5f4193b529542b84f6a9820ff262d6b691130d
Kamélia DAUDEL <kamelia.daudel@telecom-paristech.fr>
2016-06-22 11:56:51 +0200

    Dijkstra

--- FriendshipIsMagic/jni/ai/matrix/matrix.h
+++ FriendshipIsMagic/jni/ai/matrix/matrix.h
@@ -22,7 +22,6 @@ public:
 	std::vector<VertexInterface*> getAllVertices() override;
 	int getWeight(VertexInterface* src, VertexInterface* dst) override;
 	std::pair<unsigned int, unsigned int> MatrixToCoordinates(unsigned int S);
-private:
 	vector<char> mChar;
 	vector<VertexInterface*> mVertices;
 	unsigned int lengthX;
